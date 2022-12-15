e0cb51e7235694f0152069607388afa2d1811f87
Christoph Beickler <bei@ichaus.de>
2022-12-15 14:57:43 +0100

    aufgeräumt, define debug eingeführt

--- read_sensor_0v1/read_sensor_0v1.ino
+++ read_sensor_0v1/read_sensor_0v1.ino
@@ -1,14 +1,15 @@
-
+//replace standart arduino ethernet library with
+//this one https://github.com/WIZnet-ArduinoEthernet/Ethernet
+//for this to work
+//set values for IP address and MAC address
 
 #include <DHT.h>
 #include <vector>
 #include <SPI.h>
 #include <Ethernet.h>
 
-//replace ethernet library with
-//this one https://github.com/WIZnet-ArduinoEthernet/Ethernet
-//for this to work
 
+#define DEBUG
 
 // Network config//
 byte mac[] = {
@@ -18,7 +19,7 @@ IPAddress ip(192, 168, 1, 2);
 EthernetServer server(80);
 
 
-// Sensor config//
+// Sensor config
 #define DHTPIN 2           //Input pin
 #define DHTTYPE DHT22      //Sensor type
 DHT dht(DHTPIN, DHTTYPE);  //creates DHT object
@@ -26,10 +27,15 @@ DHT dht(DHTPIN, DHTTYPE);  //creates DHT object
 
 //value storage
 float hum;  //Stores humidity value
+bool error = false;
 std::vector<float> werte;
+std::vector<float>::iterator i;
 
-//debug
+
+#ifdef DEBUG
 bool client_was_here = false;
+#endif // DEBUG
+
 
 void hum_average() {
   float avr = 0;
@@ -44,17 +50,14 @@ void hum_average() {
 
 void setup() {
 
-  Ethernet.init(17);
-  //debug, remove
-  Serial.begin(9600);
-
-  //time for serial monitor to kick in
-  delay(5000);
-  Serial.println(client_was_here);
-  //Start an Ethernet object
-
-  delay(500);
-  Ethernet.begin(mac, ip);
+  #ifdef DEBUG
+    Serial.begin(9600);     //time for serial monitor to kick in  
+    delay(5000);
+    Serial.println(client_was_here);
+  #endif
+  
+  Ethernet.init(17);        //Use pin 17 for CS
+  Ethernet.begin(mac, ip);  //Start an Ethernet object
   delay(500);
 
 
@@ -81,89 +84,88 @@ void setup() {
     delay(500);
   }
 
-
-
-
   if (Ethernet.linkStatus() != LinkON) {
     Serial.println("Most likely no networkcable");
   } else {
     Serial.println("Most likely networkcabel");
   }
-
   //End debug
 
-  //start server
-  server.begin();
-
-  dht.begin();    //start dht instance
-  werte.clear();  //clear vector, unnecessery?
+  server.begin();       //start server
+  dht.begin();          //start dht instance
+  werte.clear();        //clear vector, unnecessery?
 }
 
 
 void loop() {
 
+  // Trimm length of vector
   if (werte.size() >= 10) {
     werte.erase(werte.begin(), werte.begin() + 2);
   }
-  //Read data and store it to variables hum and temp
-  hum = dht.readHumidity();
-
-  hum != hum ? Serial.println("Oh no") : Serial.println("Go on");
-
-  werte.push_back(hum);
-  std::vector<float>::iterator i = werte.end();
-  i--;
-
-  // delay(500);  //Delay 2 sec.
-
-  Serial.print("Humidity: ");
-  Serial.println(*i);
-  hum_average();
-
-  delay(500);
-  Serial.println("jetzt kommt der server shit");
-
-  //listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    client_was_here = true;
-    bool currentLineIsBlank = true;  //necessary?
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard HTTP response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");         // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          //writing the value
-          client.print(*i);
-          client.println("<br />");
-          client.println("</html>");
-          break;
-        }
-
-        //important?
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(50);
-    // close the connection:
-  } else {
-    Serial.println("NOOOOOOOOOOOOOOOOOOOOOO");
+   
+  hum = dht.readHumidity();     //Read data and store it to variable hum
+  if(hum != hum){
+    error = true;
+  }else{
+    error = false;
+    werte.push_back(hum);
+    i = werte.end();
+    i--;
   }
-  client.stop();
-  Serial.println(client_was_here);
+
+#ifdef DEBUG
+  if(error){
+    Serial.println("could not read data from sensor");
+  }else{
+    Serial.print("Humidity: ");
+    Serial.println(*i);
+    hum_average();
+    Serial.println("jetzt kommt der server shit");
+  }
+#endif // DEBUG
+
+  // //listen for incoming clients
+  // EthernetClient client = server.available();
+  // if (client) {
+  //   client_was_here = true;
+  //   bool currentLineIsBlank = true;  //necessary?
+  //   while (client.connected()) {
+  //     if (client.available()) {
+  //       char c = client.read();
+  //       if (c == '\n' && currentLineIsBlank) {
+  //         // send a standard HTTP response header
+  //         client.println("HTTP/1.1 200 OK");
+  //         client.println("Content-Type: text/html");
+  //         client.println("Connection: close");  // the connection will be closed after completion of the response
+  //         client.println("Refresh: 5");         // refresh the page automatically every 5 sec
+  //         client.println();
+  //         client.println("<!DOCTYPE HTML>");
+  //         client.println("<html>");
+  //         //writing the value
+  //         client.print(*i);
+  //         client.println("<br />");
+  //         client.println("</html>");
+  //         break;
+  //       }
+
+  //       //important?
+  //       if (c == '\n') {
+  //         // you're starting a new line
+  //         currentLineIsBlank = true;
+  //       } else if (c != '\r') {
+  //         // you've gotten a character on the current line
+  //         currentLineIsBlank = false;
+  //       }
+  //     }
+  //   }
+  //   // give the web browser time to receive the data
+  //   delay(50);
+  //   // close the connection:
+  // } else {
+  //   Serial.println("NOOOOOOOOOOOOOOOOOOOOOO");
+  // }
+  // client.stop();
+  // Serial.println(client_was_here);
   delay(5000);
 }
